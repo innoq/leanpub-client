9464961b87e9587275e72a50063093ad00db16ad
Tina Schoenborn <tina.schoenborn@innoq.com>
2016-02-15 14:44:37 +0100

    Refactoring

--- src/main/scala/LeanPubClient.scala
+++ src/main/scala/LeanPubClient.scala
@@ -13,6 +13,31 @@ class LeanPubClient(http: HttpExt, apiKey: String)(implicit materializer: Materi
   val host: String = "https://leanpub.com"
   val urlCodec: URLCodec = new URLCodec()
 
+  def post(uri: Uri, formParams: Map[String, String] = Map.empty): Future[Unit] = {
+    val formData = FormData(formParams + ("api_key" -> apiKey))
+    val request = HttpRequest(uri = uri, method = HttpMethods.POST, entity = formData.toEntity)
+    http.singleRequest(request).flatMap(handleResponseToPost)
+  }
+
+  def get(uri: Uri): Future[Unit] = {
+    val request = HttpRequest(uri = uri, method = HttpMethods.POST, entity = FormData("api_key" -> apiKey).toEntity)
+    http.singleRequest(request).flatMap(handleResponseToGet)
+  }
+
+  def handleResponseToPost(response: HttpResponse): Future[Unit] = {
+    response.status match {
+      case StatusCodes.OK => Future.successful(())
+      case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
+    }
+  }
+
+  def handleResponseToGet(response: HttpResponse): Future[Unit] = {
+    response.status match {
+      case StatusCodes.OK => Future.successful(response.entity)
+      case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
+    }
+  }
+
   def triggerPreview(slug: String): Future[Unit] = post(Uri(s"$host/$slug/preview.json"))
 
   def triggerPublish(slug: String, emailText: Option[String]): Future[Unit] = {
@@ -23,41 +48,19 @@ class LeanPubClient(http: HttpExt, apiKey: String)(implicit materializer: Materi
     post(Uri(s"$host/$slug/publish.json"), formParams)
   }
 
-  def handleResponse(response: HttpResponse): Future[Unit] = {
-    response.status match {
-      case StatusCodes.OK => Future.successful(())
-      case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
-    }
-  }
-
-  def post(uri: Uri, formParams: Map[String, String] = Map.empty): Future[Unit] = {
-    val formData = FormData(formParams + ("api_key" -> apiKey))
-    val request = HttpRequest(uri = uri, method = HttpMethods.POST, entity = formData.toEntity)
-    http.singleRequest(request).flatMap(handleResponse)
-  }
-
-  def get(uri: String): Future[Unit] = {
-    http.singleRequest(HttpRequest(uri=uri, method=HttpMethods.GET, entity=FormData("api_key" -> apiKey).toEntity)).flatMap { response =>
-      response.status match {
-        case StatusCodes.OK => Future.successful(response.entity)
-        case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
-      }
-    }
-  }
-
   def getCoupons(slug: String): Future[Unit] = {
-    get(s"$host/$slug/coupons.json")
+    get(Uri(s"$host/$slug/coupons.json"))
   }
 
   def getSummary(slug: String): Future[Unit] = {
-    get(s"$host/$slug.json")
+    get(Uri(s"$host/$slug.json"))
   }
 
   def getSales(slug: String): Future[Unit] = {
-    get(s"$host/$slug/sales.json")
+    get(Uri(s"$host/$slug/sales.json"))
   }
 
   def getIndividualPurchases(slug: String): Future[Unit] = {
-    get(s"$host/$slug/individual_purchases.json")
+    get(Uri(s"$host/$slug/individual_purchases.json"))
   }
 }
