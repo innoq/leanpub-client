1dac813b401cf71d361e9a8391f722926adbb475
Tina Schoenborn <tina.schoenborn@innoq.com>
2016-02-15 17:07:58 +0100

    Add coupon method and make methods private

--- src/main/scala/LeanPubClient.scala
+++ src/main/scala/LeanPubClient.scala
@@ -13,25 +13,25 @@ class LeanPubClient(http: HttpExt, apiKey: String)(implicit materializer: Materi
   val host: String = "https://leanpub.com"
   val urlCodec: URLCodec = new URLCodec()
 
-  def post(uri: Uri, formParams: Map[String, String] = Map.empty): Future[Unit] = {
+  private def post(uri: Uri, formParams: Map[String, String] = Map.empty): Future[Unit] = {
     val formData = FormData(formParams + ("api_key" -> apiKey))
     val request = HttpRequest(uri = uri, method = HttpMethods.POST, entity = formData.toEntity)
     http.singleRequest(request).flatMap(handleResponseToPost)
   }
 
-  def get(uri: Uri): Future[Unit] = {
-    val request = HttpRequest(uri = uri, method = HttpMethods.POST, entity = FormData("api_key" -> apiKey).toEntity)
+  private def get(uri: Uri): Future[Unit] = {
+    val request = HttpRequest(uri = uri, method = HttpMethods.GET, entity = FormData("api_key" -> apiKey).toEntity)
     http.singleRequest(request).flatMap(handleResponseToGet)
   }
 
-  def handleResponseToPost(response: HttpResponse): Future[Unit] = {
+  private def handleResponseToPost(response: HttpResponse): Future[Unit] = {
     response.status match {
       case StatusCodes.OK => Future.successful(())
       case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
     }
   }
 
-  def handleResponseToGet(response: HttpResponse): Future[Unit] = {
+  private def handleResponseToGet(response: HttpResponse): Future[ResponseEntity] = {
     response.status match {
       case StatusCodes.OK => Future.successful(response.entity)
       case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
@@ -46,6 +46,22 @@ class LeanPubClient(http: HttpExt, apiKey: String)(implicit materializer: Materi
       case None => Map.empty[String, String]
     }
     post(Uri(s"$host/$slug/publish.json"), formParams)
+  }
+
+  def createCoupon(slug: String,
+                   couponCode: String,
+                   discountedPrice: Float,
+                   startDate: DateTime,
+                   endDate: DateTime,
+                   maxUses: Int = 0,
+                   note: String = ""): Future[Unit] = {
+    val formParams = Map("coupon[coupon_code]" -> urlCodec.encode(couponCode),
+                         "coupon[package_discounts_attributes]" -> Array(s"{package_slug: $slug, discounted_price: $discountedPrice}").toString,
+                         "coupon[start_date]" -> urlCodec.encode(startDate.toIsoDateString),
+                         "coupon[end_date]" -> urlCodec.encode(endDate.toIsoDateString),
+                         "coupn[max_uses]" -> urlCodec.encode(maxUses.toString),
+                         "coupon[note]" -> urlCodec.encode(note))
+    post(Uri(s"$host/$slug/coupons.json"), formParams)
   }
 
   def getCoupons(slug: String): Future[Unit] = {
