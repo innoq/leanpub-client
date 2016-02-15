fdf3786181da6aa899fee2d17b62a58281ef8c5c
Tina Schoenborn <tina.schoenborn@innoq.com>
2016-02-15 14:15:19 +0100

    Refactor code and add url-encoding

--- src/main/scala/LeanPubClient.scala
+++ src/main/scala/LeanPubClient.scala
@@ -3,7 +3,7 @@ import akka.http.scaladsl.model._
 import akka.stream.Materializer
 
 import scala.concurrent.{ExecutionContext, Future}
-
+import org.apache.commons.codec.net.URLCodec
 
 /**
   * Created by tina on 25.01.16.
@@ -11,31 +11,30 @@ import scala.concurrent.{ExecutionContext, Future}
 class LeanPubClient(http: HttpExt, apiKey: String)(implicit materializer: Materializer, executionContext: ExecutionContext) {
 
   val host: String = "https://leanpub.com"
+  val urlCodec: URLCodec = new URLCodec()
 
-  def triggerPreview(slug: String): Future[Unit] = {
-    http.singleRequest(HttpRequest(uri=s"$host/$slug/preview.json", method=HttpMethods.POST, entity=FormData("api_key" -> apiKey).toEntity)).flatMap { response =>
-        response.status match {
-          case StatusCodes.OK => Future.successful(())
-          case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
-      }
-    }
-  }
+  def triggerPreview(slug: String): Future[Unit] = post(Uri(s"$host/$slug/preview.json"))
 
   def triggerPublish(slug: String, emailText: Option[String]): Future[Unit] = {
-    val request: HttpRequest = emailText match {
-      case Some(text) => HttpRequest( uri=s"$host/$slug/publish.json",
-                                      method=HttpMethods.POST,
-                                      entity=FormData("api_key" -> apiKey, "publish[email_readers]" -> "true", "publish[release_notes]" -> text).toEntity )
-      case None => HttpRequest( uri=s"$host/$slug/publish.json",
-                                method=HttpMethods.POST,
-                                entity=FormData("api_key" -> apiKey).toEntity )
+    val formParams = emailText match {
+      case Some(text) => Map("publish[email_readers]" -> "true", "publish[release_notes]" -> urlCodec.encode(text))
+      case None => Map.empty[String, String]
     }
-    http.singleRequest(request).flatMap { response =>
-      response.status match {
-        case StatusCodes.OK => Future.successful(())
-        case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
-      }
+    post(Uri(s"$host/$slug/publish.json"), formParams)
+  }
+
+  def handleResponse(response: HttpResponse): Future[Unit] = {
+    response.status match {
+      case StatusCodes.OK => Future.successful(())
+      case code => Future.failed(new RuntimeException(s"Request to $host failed, Statuscode: $code"))
     }
   }
+
+  def post(uri: Uri, formParams: Map[String, String] = Map.empty): Future[Unit] = {
+    val formData = FormData(formParams + ("api_key" -> apiKey))
+    val request = HttpRequest(uri = uri, method = HttpMethods.POST, entity = formData.toEntity)
+    http.singleRequest(request).flatMap(handleResponse)
+  }
+
 
 }
