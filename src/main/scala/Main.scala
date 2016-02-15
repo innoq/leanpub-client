a76014c01f1d87f988fec2d3062ac7de5e93bb67
Tina Schoenborn <tina.schoenborn@innoq.com>
2016-02-15 17:44:32 +0100

    Test book summary in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import play.api.libs.json.Json
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -14,7 +15,9 @@ object Main extends App {
   val http = Http()
 
   val client = new LeanPubClient(http, sys.env("LEANPUB_API_KEY"))
-  Await.result(client.triggerPreview("notabook77"), 5.seconds)
-  Await.result(client.triggerPublish("notabook77", Some("hello World")), 5.seconds)
+  //Await.result(client.triggerPreview("notabook77"), 5.seconds)
+  //Await.result(client.triggerPublish("notabook77", Some("hello World")), 5.seconds)
+  val response = client.getSummary("notabook77")
+  println(Json.prettyPrint(Await.result(response, 5.seconds)))
   http.shutdownAllConnectionPools() andThen { case _ => system.shutdown() }
 }
