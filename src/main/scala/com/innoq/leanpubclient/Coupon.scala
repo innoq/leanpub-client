c8a8b945f6e9738e1a4c23cfc630b968ce891af0
Tina Schoenborn <tina.schoenborn@innoq.com>
2016-03-07 19:52:55 +0100

    Use PackageDiscount Class

--- src/main/scala/com/innoq/leanpubclient/Coupon.scala
+++ src/main/scala/com/innoq/leanpubclient/Coupon.scala
@@ -4,11 +4,9 @@ import java.time.ZonedDateTime
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
-import scala.collection.immutable.HashMap
-
 case class Coupon(couponCode: String,
                   createdAt: ZonedDateTime,
-                  //packageDiscounts: Array[HashMap],
+                  packageDiscounts: List[PackageDiscount],
                   endDate: ZonedDateTime,
                   maxUses: Option[Int],
                   note: String,
@@ -22,15 +20,15 @@ case class Coupon(couponCode: String,
 object Coupon {
   implicit val couponReads: Reads[Coupon] = (
       (JsPath \ "coupon_code").read[String] and
-        (JsPath \ "created_at").read[ZonedDateTime] and
-        //(JsPath \ "package_discounts").read[]
-        (JsPath \ "end_date").read[ZonedDateTime] and
-        (JsPath\ "max_uses").readNullable[Int] and
-        (JsPath \ "note").read[String] and
-        (JsPath \ "num_uses").read[Int] and
-        (JsPath \ "start_date").read[ZonedDateTime] and
-        (JsPath \ "suspended").read[Boolean] and
-        (JsPath \ "updated_at").read[ZonedDateTime] and
-        (JsPath \ "book_slug").read[String]
+      (JsPath \ "created_at").read[ZonedDateTime] and
+      (JsPath \ "package_discounts").read[List[PackageDiscount]] and
+      (JsPath \ "end_date").read[ZonedDateTime] and
+      (JsPath\ "max_uses").readNullable[Int] and
+      (JsPath \ "note").read[String] and
+      (JsPath \ "num_uses").read[Int] and
+      (JsPath \ "start_date").read[ZonedDateTime] and
+      (JsPath \ "suspended").read[Boolean] and
+      (JsPath \ "updated_at").read[ZonedDateTime] and
+      (JsPath \ "book_slug").read[String]
     ) (Coupon.apply _)
 }
