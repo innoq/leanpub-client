a8ec65dd610c4a134b1370063672216ec21220a4
Tina Schoenborn <tina.schoenborn@innoq.com>
2016-03-30 14:51:17 +0200

    Add json writes for PackageDiscount

--- src/main/scala/com/innoq/leanpubclient/PackageDiscount.scala
+++ src/main/scala/com/innoq/leanpubclient/PackageDiscount.scala
@@ -6,7 +6,12 @@ import play.api.libs.json._
 case class PackageDiscount(identifier: String, discountedPrice: BigDecimal)
 object PackageDiscount {
   implicit val packageDiscountReads: Reads[PackageDiscount] = (
-    (JsPath \ "package_slug").read[String] and
+      (JsPath \ "package_slug").read[String] and
       (JsPath \ "discounted_price").read[BigDecimal]
+    ) (PackageDiscount.apply _)
+
+  implicit val packageDiscountWrites: Writes[PackageDiscount] = (
+      (JsPath \ "package_slug").write[String] and
+      (JsPath \ "discounted_price").write[BigDecimal]
     ) (PackageDiscount.apply _)
 }
