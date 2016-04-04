d5209ec5627f009d49efe6d93b57063c902a583b
Tina Schoenborn <tina.schoenborn@innoq.com>
2016-04-04 16:23:37 +0200

    Add assertion to test Coupon Json

--- src/test/scala/com/innoq/leanpubclient/CouponJsonSpec.scala
+++ src/test/scala/com/innoq/leanpubclient/CouponJsonSpec.scala
@@ -3,6 +3,7 @@ package com.innoq.leanpubclient
 import org.scalatest.WordSpec
 import play.api.libs.json._
 import play.libs.Json
+import java.time.ZonedDateTime
 
 class CouponJsonSpec extends WordSpec {
 
@@ -10,7 +11,7 @@ class CouponJsonSpec extends WordSpec {
     "reading JSON" which {
       "is valid and contains all Coupon Data" should {
         "return a Coupon Object with all fields" in {
-          val json = """{
+          val json = s"""{
             "coupon_code": "test",
             "created_at": "2016-01-01T12:00:00Z",
             "package_discounts": [
@@ -29,6 +30,10 @@ class CouponJsonSpec extends WordSpec {
             "book_slug": "yourbook"
           }"""
           val coupon = Json.parse(json).as[Coupon]
+          assert(coupon == Coupon("test", ZonedDateTime.parse("2016-01-01T12:00:00Z"), List(PackageDiscount("book", 2.0)),
+                                  ZonedDateTime.parse("2016-05-17"), 15, "This is not a real coupon", 12,
+                                  ZonedDateTime.parse("2013-04-17"), false, ZonedDateTime.parse("2013-04-17T22:12:58Z"),
+                                  "yourbook"))
         }
       }
     }
