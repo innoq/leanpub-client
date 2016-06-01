705576bf360c5542245fd7233939dfe8b7a389dc
Tina Schoenborn <tina.schoenborn@innoq.com>
2016-06-01 17:35:08 +0200

    remove unnecessary fields for UpdateCoupon

--- src/main/scala/com/innoq/leanpubclient/UpdateCoupon.scala
+++ src/main/scala/com/innoq/leanpubclient/UpdateCoupon.scala
@@ -7,28 +7,21 @@ import play.api.libs.json._
 /**
   * Created by tina on 23.05.16.
   */
-case class UpdateCoupon(createdAt: Option[ZonedDateTime] = None,
-                        packageDiscounts: Option[List[PackageDiscount]] = None,
+case class UpdateCoupon(packageDiscounts: Option[List[PackageDiscount]] = None,
                         endDate: Option[LocalDate] = None,
                         maxUses: Option[Int] = None,
                         note: Option[String] = None,
-                        numUses: Option[Int] = None,
                         startDate: Option[LocalDate] = None,
-                        suspended: Option[Boolean] = None,
-                        updatedAt: Option[ZonedDateTime] = None,
-                        bookSlug: Option[String] = None)
+                        suspended: Option[Boolean] = None
+                        )
 
 object UpdateCoupon {
   implicit val updateCouponWrites: Writes[UpdateCoupon] = (
-    (JsPath \ "created_at").writeNullable[ZonedDateTime] and
       (JsPath \ "package_discounts").writeNullable[List[PackageDiscount]] and
       (JsPath \ "end_date").writeNullable[LocalDate] and
-      (JsPath\ "max_uses").writeNullable[Int] and
+      (JsPath \ "max_uses").writeNullable[Int] and
       (JsPath \ "note").writeNullable[String] and
-      (JsPath \ "num_uses").writeNullable[Int] and
       (JsPath \ "start_date").writeNullable[LocalDate] and
-      (JsPath \ "suspended").writeNullable[Boolean] and
-      (JsPath \ "updated_at").writeNullable[ZonedDateTime] and
-      (JsPath \ "book_slug").writeNullable[String]
+      (JsPath \ "suspended").writeNullable[Boolean]
     ) (unlift(UpdateCoupon.unapply))
 }
