513139510aa8c1e4cbfad90cea47560a6340a595
Tina Schoenborn <tina.schoenborn@innoq.com>
2017-02-14 15:25:54 +0100

    add scaladoc

--- src/main/scala/com/innoq/leanpubclient/Result.scala
+++ src/main/scala/com/innoq/leanpubclient/Result.scala
@@ -2,6 +2,14 @@ package com.innoq.leanpubclient
 
 import play.api.libs.json.JsValue
 
+/** Result is a custom response to a POST request.
+  *
+  * [[Result.Success]] signifies a successful POST request.
+  * A [[Result.ClientError]] can occur if you perform a request that is not valid by the terms
+  * of the Leanpub API. An example of an invalid request is a second POST request with
+  * the same coupon code via [[LeanPubClient.createCoupon]].
+  * A [[Result.NotFoundError]] occurs when the client receives a 404 response on a POST request.
+  */
 sealed trait Result
 object Result {
   case object Success extends Result
--- src/main/scala/com/innoq/leanpubclient/UnexpectedStatusException.scala
+++ src/main/scala/com/innoq/leanpubclient/UnexpectedStatusException.scala
@@ -1,6 +1,11 @@
 package com.innoq.leanpubclient
 
-
+/** An UnexpectedStatusException will be thrown on a HTTP status code different from 200 or 404
+  * by [[ResponseHandler]] after a POST or GET request.
+  *
+  * @param url requested url
+  * @param code response HTTP status code
+  */
 case class UnexpectedStatusException(url: String, code: Int) extends Exception {
 
   def message: String = s"Request to $url failed, Statuscode: $code"
