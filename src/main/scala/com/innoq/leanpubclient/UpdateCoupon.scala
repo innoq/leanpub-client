f7fcbeff6caa282e179023c8e09668b4be055e34
Tina Schoenborn <tina.schoenborn@innoq.com>
2017-03-07 12:22:53 +0100

    add MaxUses trait

--- src/main/scala/com/innoq/leanpubclient/UpdateCoupon.scala
+++ src/main/scala/com/innoq/leanpubclient/UpdateCoupon.scala
@@ -1,6 +1,8 @@
 package com.innoq.leanpubclient
 
 import java.time.LocalDate
+
+import com.innoq.leanpubclient.UpdateCoupon.MaxUses
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
@@ -18,17 +20,29 @@ import play.api.libs.json._
 case class UpdateCoupon(packageDiscounts: Option[List[PackageDiscount]] = None,
                         startDate: Option[LocalDate] = None,
                         endDate: Option[LocalDate] = None,
-                        maxUses: Option[Int] = None,
+                        maxUses: Option[MaxUses] = None,
                         note: Option[String] = None,
                         suspended: Option[Boolean] = None
                         )
 
 object UpdateCoupon {
+
+  sealed trait MaxUses
+  object MaxUses {
+    case object UnlimitedUses extends MaxUses
+    case class LimitedUses(numberMaxUses: Int) extends MaxUses
+
+    implicit val maxUsesWrites: Writes[MaxUses] = Writes {
+      case UnlimitedUses => JsNull
+      case LimitedUses(numberMaxUses) => JsNumber(numberMaxUses)
+    }
+  }
+
   implicit val updateCouponWrites: Writes[UpdateCoupon] = (
       (JsPath \ "package_discounts_attributes").writeNullable[List[PackageDiscount]] and
       (JsPath \ "start_date").writeNullable[LocalDate] and
       (JsPath \ "end_date").writeNullable[LocalDate] and
-      (JsPath \ "max_uses").writeNullable[Int] and
+      (JsPath \ "max_uses").writeNullable[MaxUses] and
       (JsPath \ "note").writeNullable[String] and
       (JsPath \ "suspended").writeNullable[Boolean]
     ) (unlift(UpdateCoupon.unapply))
